3240da6b575d278a500488b2ffdccf36a81d1e4f
Anmol Dash <dash2@umbc.edu>
2023-11-04 10:47:08 -0400

    feat: Adding Vercel Analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.scss";
 import Main from "./containers/Main";
+import { inject } from '@vercel/analytics';
+
 
 function App() {
   return (
@@ -9,5 +11,5 @@ function App() {
     </div>
   );
 }
-
+inject();
 export default App;
